99f624a3325b140739c2925f3bf8378633b19e6a
SaiPrakash-voldemort <saiprakashsai10@gmail.com>
2025-06-24 19:07:36 +0530

    first commit

--- backend/gemini_test.py
+++ backend/gemini_test.py
@@ -1,7 +1,7 @@
 
 from google import genai
 
-client = genai.Client(api_key="AIzaSyA2WdMoDZD05CQciBv1D43mk-soCU6k48Q")
+client = genai.Client(api_key="")
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works in a few words"
